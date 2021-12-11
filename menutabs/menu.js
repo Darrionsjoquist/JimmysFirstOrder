eed5b8c434552e4da533ec1bb6dab26d3aec425d
bOtChuMHATESThEsIklEbUnwiTcH <achinn@nwlink.com>
2021-12-11 11:15:35 -0800

    menutab temlate folder very cool done by you truly very quality

---
+++ menutabs/menu.js
@@ -0,0 +1,13 @@
+let Menu = [
+{
+EntryName:"srimp paste bowl",
+EntryType:"Food",
+EntryDiscription:"freshly frozen shrimp ground into paste",
+EntryPrice:"$160"
+},
+{EntryName:"paste cup",
+EntryType:"Drinks",
+EntryDiscription:"freshly frozen shrimp ground into paste",
+EntryPrice:"$160"
+}
+]
---
+++ menutabs/mo.js
@@ -0,0 +1,43 @@
+let foodQ = document.getElementById(`Food`)
+let drinksQ = document.getElementById(`Drinks`)
+let specialsQ = document.getElementById(`Specials`)
+
+let MenuList = document.getElementById(`MenuList`)
+
+function GenFoodEntry(e){
+return `
+<div>
+      <div>
+         <h3>${e.EntryName}</h3>
+         <h3>${e.EntryPrice}</h3>
+      </div>
+      <div>
+         <p>${e.EntryDiscription}</p>
+      </div>
+
+   </div>
+`
+
+
+}
+
+
+function GenFoodMenu(e){
+   FilteredMenu = Menu.filter(function(x){
+     return x.EntryType === e.currentTarget.id
+   })
+
+   let MenuString = ""
+   for(let Entry of FilteredMenu){
+      MenuString = MenuString + GenFoodEntry(Entry)
+   }
+   
+   MenuList.innerHTML = MenuString
+
+}
+
+foodQ.addEventListener(`click`, GenFoodMenu)
+
+drinksQ.addEventListener(`click`, GenFoodMenu)
+
+specialsQ.addEventListener(`click`, GenFoodMenu)

9cd59516a106e1b6d9a912cb88ed126fd25170d6
bOtChuMHATESThEsIklEbUnwiTcH <achinn@nwlink.com>
2021-12-13 13:17:23 -0800

    hsfjgfkj

--- home.js
+++ home.js
@@ -1,3 +1,30 @@
 let menuLink = document.getElementById("menuLink")
-let
-let
+// let aboutLink = document.getElementById("aboutLink")
+// let visitLink = document.getElementById("visitLink")
+// let uberdiv = document.getElementById("uberdiv")
+
+// function getMenuImg (e){
+// uberdiv.style.backgroundImage = url("https://i.pinimg.com/originals/76/f1/2b/76f12beb50a14e33067f91853c171728.png");
+
+// }
+
+
+
+
+
+
+
+
+
+
+
+
+menuLink.addEventListener("mouseover", (event) => {
+   // highlight the mouseover target
+   event.target.style.color = "red";
+});
+
+//menuLink.addEventListener("mouseover", getMenuImg)
+
+
+

98f295d6d08094e9468bfc6846e58217c674f15e
bOtChuMHATESThEsIklEbUnwiTcH <achinn@nwlink.com>
2021-12-14 13:04:40 -0800

    asdasd

--- zzstuff/menutabs/menu.js
+++ zzstuff/menutabs/menu.js
@@ -8,12 +8,17 @@ EntryPrice:"$160"
 {EntryName:"paste cup",
 EntryType:"Drinks",
 EntryDiscription:"freshly frozen shrimp ground into paste",
-EntryPrice:"$160"
+EntryPrice:"$154"
 },
-{EntryName:"paste cup",
+{EntryName:"glop glass",
+EntryType:"Drinks",
+EntryDiscription:"grean glop from fresh the drain",
+EntryPrice:"$57"
+},
+{EntryName:"candied paste",
 EntryType:"Treats",
 EntryDiscription:"freshly frozen shrimp ground into paste",
-EntryPrice:"$160"}
+EntryPrice:"$28"}
 ]
 
 
@@ -26,14 +31,14 @@ specialImg:"https://i.redd.it/6as3k6sprar51.jpg"
 },
 
 {
-   specialName:"hat monkey",
-   specialDisc:"this monkey is one of the most cultured around",
+   specialName:"Hat Monkey",
+   specialDisc:"this monkey is one of the most cultured around, his class and civility are unmached",
    specialImg:"https://thumbs.dreamstime.com/b/closeup-illustration-primate-monkey-wearing-black-bowler-hat-cap-computer-generated-illustration-image-head-163807690.jpg"
 },
 
 {
-   specialName:"thug monkey",
-   specialDisc:"the thug monkey is king of the streets",
+   specialName:"Thug Monkey",
+   specialDisc:"the thug monkey is king of the streets, better pay your debts or he'll rip yer arms off",
    specialImg:"https://p1.pxfuel.com/preview/882/848/948/monkey-cool-stress-cigarette-sit-sunglasses.jpg"
 }
 ]

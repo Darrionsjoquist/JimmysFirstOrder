ce18d5c1fe225ef885896df74fc0d2ecb8cdc94a
Anne Chinn <achinn@nwlink.com>
2021-12-23 12:47:10 -0800

    updated home

--- home.js
+++ home.js
@@ -1,28 +1,13 @@
-let menuLink = document.getElementById("menuLink")
- let aboutLink = document.getElementById("aboutLink")
- let visitLink = document.getElementById("visitLink")
- let main = document.getElementById("main")
+function addHoverEvent(linkId, imageUrl) {
+   document.getElementById(linkId).addEventListener("mouseover", (event) => {
+      document.getElementById("main").style.backgroundImage = `url(${imageUrl})`
+   });
+}
 
+function setupLinks() {
+   addHoverEvent("menuLink",  "https://media.istockphoto.com/photos/young-sad-man-thinking-while-sitting-on-a-bench-in-autumn-picture-id884811730");
+   addHoverEvent("aboutLink", "https://l450v.alamy.com/450v/2c598pc/young-bearded-man-thinking-too-hard-solving-a-difficult-problem-angry-stressed-guy-with-headache-isolated-on-grey-studio-background-2c598pc.jpg");
+   addHoverEvent("visitLink", "https://cdn.xxl.thumbs.canstockphoto.com/angry-man-on-the-phones-stock-image_csp41564063.jpg");
+}
 
-
-
-
-
-
-
-
-
-
-
-
-menuLink.addEventListener("mouseover", (event) => {
-   uberdiv.style.backgroundImage = "url(https://media.istockphoto.com/photos/young-sad-man-thinking-while-sitting-on-a-bench-in-autumn-picture-id884811730)"
-});
-
-aboutLink.addEventListener("mouseover", (event) => {
-   uberdiv.style.backgroundImage = "url(https://l450v.alamy.com/450v/2c598pc/young-bearded-man-thinking-too-hard-solving-a-difficult-problem-angry-stressed-guy-with-headache-isolated-on-grey-studio-background-2c598pc.jpg)"
-});
-
-visitLink.addEventListener("mouseover", (event) => {
-   uberdiv.style.backgroundImage = "url(https://cdn.xxl.thumbs.canstockphoto.com/angry-man-on-the-phones-stock-image_csp41564063.jpg)"
-});
+setupLinks();

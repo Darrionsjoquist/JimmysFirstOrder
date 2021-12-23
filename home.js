c79f3f3322c7d2dc9883d1b143928b2c862d9da1
Anne Chinn <achinn@nwlink.com>
2021-12-23 12:59:16 -0800

    updated home

--- home.js
+++ home.js
@@ -2,12 +2,16 @@ function addHoverEvent(linkId, imageUrl) {
    document.getElementById(linkId).addEventListener("mouseover", (event) => {
       document.getElementById("main").style.backgroundImage = `url(${imageUrl})`
    });
+
+   document.getElementById(linkId).addEventListener("mouseout", (event) => {
+      document.getElementById("main").style.backgroundImage = `url(images/salad.jpg)`
+   });
 }
 
 function setupLinks() {
-   addHoverEvent("menuLink",  "https://media.istockphoto.com/photos/young-sad-man-thinking-while-sitting-on-a-bench-in-autumn-picture-id884811730");
-   addHoverEvent("aboutLink", "https://l450v.alamy.com/450v/2c598pc/young-bearded-man-thinking-too-hard-solving-a-difficult-problem-angry-stressed-guy-with-headache-isolated-on-grey-studio-background-2c598pc.jpg");
-   addHoverEvent("visitLink", "https://cdn.xxl.thumbs.canstockphoto.com/angry-man-on-the-phones-stock-image_csp41564063.jpg");
+   addHoverEvent("menuLink",  "https://images.pexels.com/photos/1893557/pexels-photo-1893557.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1");
+   addHoverEvent("aboutLink", "https://images.pexels.com/photos/3009767/pexels-photo-3009767.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1");
+   addHoverEvent("visitLink", "https://images.pexels.com/photos/958545/pexels-photo-958545.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1");
 }
 
 setupLinks();

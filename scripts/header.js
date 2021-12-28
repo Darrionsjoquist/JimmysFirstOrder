2fcb3a1c06fc2d14bdcda36f44ba802155a3002c
unknown <chnn.jason@gmail.com>
2021-12-28 13:28:54 -0800

    the links now lead around and other things

--- scripts/header.js
+++ scripts/header.js
@@ -7,16 +7,16 @@ function genHeader(){
   </div>
   <nav> 
     <ul>
-      <li><a href="">menu</a></li>
+      <li><a href="../menu.html">menu</a></li>
       <li><a href="../about.html" >about</a></li>
       <li><a href="../visit.html">visit</a></li>
     </ul>
   </nav>
   <div class="socials">
       <ul class="social-list">
-        <li class="social-item"><a href="#" class="social-link"><i class="fab fa-facebook-square"></i></a></li>
-        <li class="social-item"><a href="#" class="social-link"><i class="fab fa-twitter"></i></a></li>
-        <li class="social-item"><a href="#" class="social-link"><i class="fab fa-instagram"></i></a></li>
+        <li class="social-item"><a href="https://www.facebook.com/jimmysonfirst" class="social-link"><i class="fab fa-facebook-square"></i></a></li>
+        <li class="social-item"><a href="https://twitter.com/JimmysOnFirst" class="social-link"><i class="fab fa-twitter"></i></a></li>
+        <li class="social-item"><a href="https://www.pinterest.com/stadiumjimmys/_created/" class="social-link"><i class="fab fa-pinterest-square"></i></i></a></li>
       </ul>
   </div>
 </div>`

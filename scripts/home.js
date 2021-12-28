4157c5d107dd84f086c955be71168a6b0eaca49a
unknown <chnn.jason@gmail.com>
2021-12-28 12:36:05 -0800

    sdaadasdasdasdasdssss

--- scripts/home.js
+++ scripts/home.js
@@ -4,14 +4,14 @@ function addHoverEvent(linkId, imageUrl) {
    });
 
    document.getElementById(linkId).addEventListener("mouseout", (event) => {
-      document.getElementById("main").style.backgroundImage = `url(images/salad.jpg)`
+      document.getElementById("main").style.backgroundImage = `url(images/salade.png)`
    });
 }
 
 function setupLinks() {
-   addHoverEvent("menuLink",  "https://images.pexels.com/photos/1893557/pexels-photo-1893557.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1");
-   addHoverEvent("aboutLink", "https://images.pexels.com/photos/3009767/pexels-photo-3009767.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1");
-   addHoverEvent("visitLink", "https://images.pexels.com/photos/958545/pexels-photo-958545.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1");
+   addHoverEvent("menuLink",  "images/twinky.png");
+   addHoverEvent("aboutLink", "images/tart.png");
+   addHoverEvent("visitLink", "images/spam.png");
 }
 
 setupLinks();

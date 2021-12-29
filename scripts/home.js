439f3028e151b5fa34b9417f28172b74e8bd60ad
darrionsjoquist <darrionsjoquist@gmail.com>
2021-12-29 13:02:20 -0800

    merge

--- scripts/home.js
+++ scripts/home.js
@@ -15,3 +15,29 @@ function setupLinks() {
 }
 
 setupLinks();
+
+
+
+
+   function genAddress (){
+      document.getElementById("contact").innerHTML= `
+              <div class="contact">
+                  <p>
+                  Contact<br>
+                  206-322-9734<br>
+                  1406 12th Ave<br>
+                  Seattle, WA 98122
+                  </p>
+              </div>
+      
+              <div class="hours">
+              <p>
+                  Hours<br>
+                  Tues - Sun  5pm to midnight<br>
+                  Closed Mondays
+              </p>
+              </div>
+          </div>
+      `
+      }
+      genAddress()

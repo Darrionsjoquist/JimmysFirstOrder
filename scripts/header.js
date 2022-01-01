10396f45a33750225cfc9189fea86ffdb75257c1
unknown <chnn.jason@gmail.com>
2022-01-01 12:03:09 -0800

    sdad

--- scripts/header.js
+++ scripts/header.js
@@ -11,9 +11,9 @@ function genHeader(includeNavBar = true) {
   document.getElementById("header").innerHTML = `      
   <div class="top-bar container">
   <div class="logo">
-  <a href="home.html"><img src="images/jimmys-logo.png" alt=""></a>
+    <a href="home.html"><img src="images/jimmys-logo.png" alt=""></a>
   </div>
-  ${includeNavBar?navBarHTML:''}
+  ${includeNavBar ? navBarHTML : ''}
   <div class="socials">
       <ul class="social-list">
         <li class="social-item"><a href="https://www.facebook.com/jimmysonfirst" target="_blank" class="social-link"><i class="fab fa-facebook-square"></i></a></li>
@@ -21,6 +21,22 @@ function genHeader(includeNavBar = true) {
         <li class="social-item"><a href="https://www.pinterest.com/stadiumjimmys/_created/" target="_blank" class="social-link"><i class="fab fa-pinterest-square"></i></i></a></li>
       </ul>
   </div>
+  <div class="hamburger">
+      <a href="javascript:void(0);" class="icon" onclick="openBurger()">
+          <i class="fa fa-bars"></i>
+        </a>
+  </div>
+  <div id="burger-inside">
+      <nav> 
+          <ul>
+            <li><a href="../menu.html">menu</a></li>
+            <li><a href="../about.html" >about</a></li>
+            <li><a href="../visit.html">visit</a></li>
+          </ul>
+        </nav>
+  </div>
 </div>`
 }
 
+
+document.getElementById("burger-inside")

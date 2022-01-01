d996d9310db24e8a9bce20d094b5a8112e32afdc
unknown <chnn.jason@gmail.com>
2022-01-01 13:58:05 -0800

    dasa

--- scripts/header.js
+++ scripts/header.js
@@ -1,42 +1,55 @@
 const navBarHTML = `
-  <nav> 
+  <nav class="big-nav"> 
     <ul>
       <li><a href="../menu.html">menu</a></li>
       <li><a href="../about.html" >about</a></li>
       <li><a href="../visit.html">visit</a></li>
     </ul>
-  </nav>`;
+  </nav>`
 
 function genHeader(includeNavBar = true) {
-  document.getElementById("header").innerHTML = `      
-  <div class="top-bar container">
-  <div class="logo">
-    <a href="home.html"><img src="images/jimmys-logo.png" alt=""></a>
-  </div>
-  ${includeNavBar ? navBarHTML : ''}
-  <div class="socials">
-      <ul class="social-list">
-        <li class="social-item"><a href="https://www.facebook.com/jimmysonfirst" target="_blank" class="social-link"><i class="fab fa-facebook-square"></i></a></li>
-        <li class="social-item"><a href="https://twitter.com/JimmysOnFirst" target="_blank" class="social-link"><i class="fab fa-twitter"></i></a></li>
-        <li class="social-item"><a href="https://www.pinterest.com/stadiumjimmys/_created/" target="_blank" class="social-link"><i class="fab fa-pinterest-square"></i></i></a></li>
-      </ul>
-  </div>
-  <div class="hamburger">
-      <a href="javascript:void(0);" class="icon" onclick="openBurger()">
-          <i class="fa fa-bars"></i>
-        </a>
-  </div>
-  <div id="burger-inside">
-      <nav> 
-          <ul>
-            <li><a href="../menu.html">menu</a></li>
-            <li><a href="../about.html" >about</a></li>
-            <li><a href="../visit.html">visit</a></li>
+  document.getElementById("header").innerHTML = `  
+  <div>
+      <div class="top-bar container">
+        <div class="logo">
+          <a href="home.html"><img src="images/jimmys-logo.png" alt=""></a>
+      </div>
+      ${includeNavBar ? navBarHTML : ''}
+      <div class="socials">
+          <ul class="social-list">
+            <li class="social-item"><a href="https://www.facebook.com/jimmysonfirst" target="_blank" class="social-link"><i class="fab fa-facebook-square"></i></a></li>
+            <li class="social-item"><a href="https://twitter.com/JimmysOnFirst" target="_blank" class="social-link"><i class="fab fa-twitter"></i></a></li>
+            <li class="social-item"><a href="https://www.pinterest.com/stadiumjimmys/_created/" target="_blank" class="social-link"><i class="fab fa-pinterest-square"></i></i></a></li>
           </ul>
-        </nav>
-  </div>
+      </div>
+      <div class="hamburger">
+          <a href="javascript:void(0);" class="icon" onclick="openBurger()">
+              <i class="fa fa-bars"></i>
+            </a>
+      </div>
+    </div>
+    <div id="burger-inside">
+        <div class="small-nav"> 
+            <ul>
+              <li class="small-nav-item"><a href="../menu.html">menu</a></li>
+              <li class="small-nav-item"><a href="../about.html" >about</a></li>
+              <li class="small-nav-item"><a href="../visit.html">visit</a></li>
+            </ul>
+        </div>
+    </div>
+    
 </div>`
 }
 
+const headHTML = document.getElementById("head").innerHTML
 
-document.getElementById("burger-inside")
+let i=0
+function openBurger() {
+  if (!(document.getElementById("burger-inside").className === "burger-menu")) {
+    document.getElementById("burger-inside").className = "burger-menu"
+      
+  } else {
+    document.getElementById("burger-inside").className = ""
+  }
+
+}

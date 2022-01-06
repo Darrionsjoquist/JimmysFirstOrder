5328d741cabb56cb886396e2049cc83239f12801
Anne Chinn <achinn@nwlink.com>
2022-01-06 12:27:12 -0800

    fixed header

--- scripts/home.js
+++ scripts/home.js
@@ -1,5 +1,3 @@
-
-
 function addHoverEvent(linkId, imageUrl) {
    document.getElementById(linkId).addEventListener("mouseover", (event) => {
       document.getElementById("main").style.backgroundImage = `url(${imageUrl})`
@@ -16,30 +14,28 @@ function setupLinks() {
    addHoverEvent("visitLink", "images/spam.png");
 }
 
-setupLinks();
-
-
-
-
-   function genAddress (){
-      document.getElementById("contact").innerHTML= `
-              <div class="contact">
-                  <p>
-                  Contact<br>
-                  206-322-9734<br>
-                  1406 12th Ave<br>
-                  Seattle, WA 98122
-                  </p>
-              </div>
+function genAddress () {
+   document.getElementById("contact").innerHTML= `
+            <div class="contact">
+               <p>
+               Contact<br>
+               206-322-9734<br>
+               1406 12th Ave<br>
+               Seattle, WA 98122
+               </p>
+            </div>
+   
+            <div class="hours">
+            <p>
+               Hours<br>
+               Tues - Sun  5pm to midnight<br>
+               Closed Mondays
+            </p>
+            </div>
+         </div>
+   `
+}
       
-              <div class="hours">
-              <p>
-                  Hours<br>
-                  Tues - Sun  5pm to midnight<br>
-                  Closed Mondays
-              </p>
-              </div>
-          </div>
-      `
-      }
-      genAddress()
+genAddress();
+setupLinks();
+genHeader(true);
